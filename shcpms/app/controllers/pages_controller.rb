b359bdf25203061f7a79acb245d4124535d36e73
ConorC28 <conlonconor@gmail.com>
2018-03-07 00:00:51 +0000

    some front added

--- shcpms/app/controllers/pages_controller.rb
+++ shcpms/app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
 		@content = 'This service aims to unify the understanding doctors and hospitals have of their patients, creating
 		a central of medical information';
 	end
+	
+	def patientspage
+		@title = 'Patients';
+		@content = 'This service aims to unify the understanding doctors and hospitals have of their patients, creating
+		a central of medical information';
+	end
 end
--- shcpms/config/routes.rb
+++ shcpms/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
   get 'index' => 'welcome#index'
+   
+  get 'patientspage' => 'pages#patientspage', as: 'patientspage'
   
   get 'welcome/index'
   
-  root 'welcome#index'
+  root 'welcome#index', as: 'home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
