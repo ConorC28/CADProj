2dc4fc0eb9d355207f462ee8ef1645995ac453b7
ConorC28 <conlonconor@gmail.com>
2018-03-09 21:38:43 +0000

    collapsible nav

--- shcpms/config/routes.rb
+++ shcpms/config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   
   root 'welcome#index', as: 'home'
+  
+  #resources :patients
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
